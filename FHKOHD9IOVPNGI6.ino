234c0efdab64603cc29638ee96de28dcb3842c90
Abdul Muiz Munshi <105690969+muiz03@users.noreply.github.com>
2022-12-24 06:51:54 +0530

    Add files via upload

---
+++ FHKOHD9IOVPNGI6.ino
@@ -0,0 +1,38 @@
+const int Trigger = 10;
+const int Echo = 11;
+int buzzer=7;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
+}
+
+void loop()
+{
+  long duration, cm;
+  pinMode(Trigger, OUTPUT);
+  digitalWrite(Trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trigger, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(Trigger, LOW);
+  pinMode(Echo, INPUT);
+  duration = pulseIn(Echo, HIGH);
+  cm = microsecondsToCentimeters(duration);
+
+  if(cm<=60)
+  {
+    tone(buzzer,700,90);
+  }
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  delay(1000);
+}
+
+long microsecondsToCentimeters(long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+
+
